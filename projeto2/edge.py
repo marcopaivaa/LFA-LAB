1b8844428ea80579639a65abe0c5dba02e81c038
MARCO ANTONIO PAIVA PEREIRA <152945@facens.br>
2019-03-13 22:28:41 -0300

    Projeto2

---
+++ projeto2/edge.py
@@ -0,0 +1,15 @@
+from node import *
+
+class Edge:
+    def __init__(self, fromNode, toNode):
+        self.fromNode = fromNode
+        self.toNode = toNode
+
+    def removeEdge(edge):
+        edges.remove(edge)
+
+    def setName(name):
+        self.name = name
+    
+    def addEdge(to):
+        self.edges.append(to)
---
+++ projeto2/node.py
@@ -0,0 +1,15 @@
+from edge import *
+
+class Node:
+    def __init__(self):
+        self.name = ""
+        self.edges = []   
+
+    def removeEdge(edge):
+        edges.remove(edge)
+
+    def setName(name):
+        self.name = name
+    
+    def addEdge(to):
+        self.edges.append(to)
